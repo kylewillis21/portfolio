f9a417f58ad5d6ac835397b6c520e5cacc79c545
Kyle Willis <kw586717@ohio.edu>
2024-05-10 18:11:17 -0400

    Added navbar with a basic outline

---
+++ portfolio/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import "./styles/styles.css";
+
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
