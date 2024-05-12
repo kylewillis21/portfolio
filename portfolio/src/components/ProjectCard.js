e383d67fbdcb570da37236d4461585588c44e6de
Kyle Willis <kw586717@ohio.edu>
2024-05-12 15:04:40 -0400

    made some changes to styling

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -2,22 +2,33 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
+/**
+ * ProjectCard is a React component that displays a card with project details.
+ *
+ * @param {string} title - The title of the project.
+ * @param {string} description - A brief description of the project.
+ * @param {string} buttonLink - The link to the full code of the project.
+ * @param {string} imageSource - The source of the image for the project card.
+ * @param {string} technologies - The technologies used in the project.
+ *
+ * @returns {JSX.Element} A React Card component with the provided project details.
+ */
 export default function ProjectCard({ title, description, buttonLink, imageSource, technologies }) {
     return (
-    <Card
-        style={{width: '20rem'}}
-    >
-        <Card.Img variant='top' src={imageSource} />
-        <Card.Body>   
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{description}</Card.Text>
-            <Card.Text>Technologies Used: {technologies}</Card.Text>
-        </Card.Body>
-        <Card.Footer>
-            <a href={buttonLink} target="_blank" rel="noopener noreferrer">
-                <Button variant="primary">See Full Code</Button>
-            </a>
-        </Card.Footer>
-    </Card>
+        <Card
+            style={{ width: '20rem' }}
+        >
+            <Card.Img variant='top' src={imageSource} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{description}</Card.Text>
+                <Card.Text>Technologies Used: {technologies}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <a href={buttonLink} target="_blank" rel="noopener noreferrer">
+                    <Button variant="primary">See Full Code</Button>
+                </a>
+            </Card.Footer>
+        </Card>
     );
 }
